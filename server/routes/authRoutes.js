f3a3d0dce265a1abb8aa19afd733e30612177660
farrahalyssa <idcfarrah@gmail.com>
2025-02-14 01:29:48 -0500

    deleted backend. back to zero to learn objectoriented programming in express

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { loginUser } = require('../controllers/Auth/loginUser');
-const { registerUser } = require('../controllers/Auth/registerUser');
-
-router.post('/login', loginUser);
-router.post('/register', registerUser);
-
-module.exports = router;
--- server/routes/postRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createPost } = require('../controllers/Post/createPost')
-const {getUserPosts} = require('../controllers/Post/getUserPosts')
-router.post('/createPost', createPost)
-router.get('/user/:userId/posts', getUserPosts);
-
-module.exports = router;
