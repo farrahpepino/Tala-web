4bf39ff5b37363bd489e734c0588defd71d4f679
farrahalyssa <idcfarrah@gmail.com>
2025-02-27 11:27:56 -0500

    multer for storage, fix organization later

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 
 const express = require('express');
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' }); 
 
 const cors = require('cors');
 
