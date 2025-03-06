205b573ae14f01de398a1f5a33243a87d97aa8c0
farrahalyssa <idcfarrah@gmail.com>
2025-03-06 15:29:39 -0500

    update s3 service

--- server/services/s3Service.js
+++ server/services/s3Service.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const multer = require('multer');
-const multerS3 = require('multer-s3-v3'); // Note the change here
 const { S3Client } = require('@aws-sdk/client-s3');
 
-// Create S3 client
+const storage =  multer.memoryStorage();
+const upload= multer({storage});
+
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
@@ -12,20 +13,6 @@ const s3 = new S3Client({
   },
 });
 
-// Set up multer storage using multer-s3-v3
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.AWS_BUCKET_NAME, // Set your bucket name in your .env
-    acl: 'public-read',
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: file.fieldname });
-    },
-    key: function (req, file, cb) {
-      cb(null, `profile-pictures/${Date.now()}-${file.originalname}`);
-    }
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file size limit
-});
+
 
 module.exports = { upload };
