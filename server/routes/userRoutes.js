0286572710afa0d1fcb4a1f7fbcc230794c5ae72
farrahalyssa <idcfarrah@gmail.com>
2025-03-06 16:54:51 -0500

    deleted multer

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,17 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController'); 
-const multer = require('multer');
 
-const storage = multer.memoryStorage();
 
-// Create a multer instance with the memory storage
-const upload = multer({ storage });
-
-console.log('upload:', upload);
-console.log('typeof upload:', typeof upload); // Should output 'function'
-console.log('upload.single:', upload.single);  
-// Define the routes
 router.get('/search', UserController.searchUsers);
 router.patch('/profile/:userId', UserController.updateProfile);
 router.get('/:userId', UserController.getUserData);
