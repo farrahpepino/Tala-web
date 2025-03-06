70f16b1349162fe6f5e7e704ce6f29cf8f6f9878
farrahalyssa <idcfarrah@gmail.com>
2025-03-06 15:07:33 -0500

    debugging

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,13 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController'); 
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
-
+const { upload } = require('../services/s3Service'); // Adjust path as needed
 // Define the routes
+console.log('upload', upload)
 router.get('/search', UserController.searchUsers);
 router.patch('/profile/:userId', UserController.updateProfile);
 router.get('/:userId', UserController.getUserData);
 router.delete('/:userId/delete-account', UserController.deleteAccount);
-router.post('/update-profile-picture/:userId', upload.single('profilePicture'),  UserController.uploadProfilePicture);
+router.post('/update-profile-picture/:userId', upload.single('file'),  UserController.uploadProfilePicture);
 module.exports = router;
--- server/services/s3Service.js
+++ server/services/s3Service.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
+const multer = require('multer');
+const multerS3 = require('multer-s3-v3'); // Note the change here
+const { S3Client } = require('@aws-sdk/client-s3');
 
-const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
-
+// Create S3 client
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
@@ -10,5 +12,20 @@ const s3 = new S3Client({
   },
 });
 
-module.exports = { s3, PutObjectCommand };
+// Set up multer storage using multer-s3-v3
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME, // Set your bucket name in your .env
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, `profile-pictures/${Date.now()}-${file.originalname}`);
+    }
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file size limit
+});
 
+module.exports = { upload };
