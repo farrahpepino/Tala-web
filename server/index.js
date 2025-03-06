dbdb5b1094b1bd970fa3229e9eedebcc9bce3473
farrahalyssa <idcfarrah@gmail.com>
2025-03-06 15:41:42 -0500

    updated index

--- server/index.js
+++ server/index.js
@@ -22,11 +22,12 @@ app.use(express.json());
 
 
 app.use(cors());
-
+app.options('*', cors());
 
  app.use(cors({
    origin: '*',
    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+   allowedHeaders: 'Content-Type,Authorization'
  }));
 
  const port = process.env.PORT || 5005;
