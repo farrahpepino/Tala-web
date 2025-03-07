ce8a51aaea045207318af998b0615a08b25b6c20
farrahalyssa <idcfarrah@gmail.com>
2025-03-07 18:35:59 -0500

    update index.js

--- server/index.js
+++ server/index.js
@@ -25,10 +25,10 @@ app.use(express.json());
 app.use(cors());
 
  app.use(cors({
-   origin: '*',}))
-  //  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+   origin: '*',
+   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 //    allowedHeaders: ['Content-Type', 'Authorization']
-//   }));
+  }));
 
 
 
